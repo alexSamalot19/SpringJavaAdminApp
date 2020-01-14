0edcf8d3692db855209782cf32c0d4404a84581b
alexSam <alexsamalot19@gmail.com>
2020-01-14 08:06:51 -0500

    basic functionality of an admin app that takes information from React front end and Java backend utilizing Spring Boot

---
+++ src/main/java/com/SamalotSpring/demo/student/StudentService.java
@@ -0,0 +1,40 @@
+package com.SamalotSpring.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class StudentService {
+
+    private final StudentDataAccessService studentDataAccessService;
+
+    @Autowired
+    public StudentService(StudentDataAccessService studentDataAccessService) {
+        this.studentDataAccessService = studentDataAccessService;
+    }
+
+    List<Student> getAllStudents() {
+        return studentDataAccessService.selectAllStudents();
+    }
+
+    void addNewStudent(Student student) {
+        addNewStudent(null, student);
+    }
+
+
+    void addNewStudent(UUID studentId,Student student) {
+        UUID newStudentID = Optional.ofNullable(studentId).orElse(UUID.randomUUID());
+
+        //Todo verify that emial is not taken
+        studentDataAccessService.insertStudent(newStudentID, student);
+
+    }
+
+
+
+}
